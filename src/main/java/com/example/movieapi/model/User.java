df01329f631cf8aa263272f2dea5fb67dc1afbf8
Fara Ayeesha Binti Ahmad Yusni <92418505+faraayeesha286941@users.noreply.github.com>
2024-07-01 13:47:24 +0800

    Working Spring Boot With Authentication

--- src/main/java/com/example/movieapi/model/User.java
+++ src/main/java/com/example/movieapi/model/User.java
@@ -1,14 +1,18 @@
 package com.example.movieapi.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 
     // Getters and setters
